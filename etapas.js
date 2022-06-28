67b3517104a9dfa9953c8fda93bbd629ad5c2986
Mariane Morais <marianemoraisf28@gmail.com>
2022-06-28 10:35:09 -0300

    Include files

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'José da Silva',
+                partido: 'PSDB',
+                fotos: [
+                    { URL:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Carlos dos Santos',
+                partido: 'PCdoB',
+                fotos: [
+                    { URL:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'Marco Pereira',
+                partido: 'PSDB',
+                vice: 'Maria',
+                fotos: [
+                    { URL:'22.jpg', legenda: 'Prefeito'},
+                    { URL:'22_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Luiza Silva',
+                partido: 'PT',
+                vice: 'João',
+                fotos: [
+                    { URL:'13.jpg', legenda: 'Prefeito'},
+                    { URL:'13_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
